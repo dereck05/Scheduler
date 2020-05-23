627087f3d5b372795075a14210df3a34cf736378
dereck5 <dereck.benavides@schoolmint.com>
2020-05-23 15:28:03 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TP2_Operativos.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/dereck/CLionProjects/TP2_Operativos/main.c" "/Users/dereck/CLionProjects/TP2_Operativos/cmake-build-debug/CMakeFiles/TP2_Operativos.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/libffi/3.3/include"
+  "/usr/local/Cellar/gtk+3/3.24.18_1/include/gtk-3.0"
+  "/usr/local/Cellar/glib/2.64.3/include/gio-unix-2.0"
+  "/usr/local/Cellar/cairo/1.16.0_3/include/cairo"
+  "/usr/local/Cellar/libepoxy/1.5.4_1/include"
+  "/usr/local/Cellar/pango/1.44.7/include/pango-1.0"
+  "/usr/local/Cellar/fribidi/1.0.9/include/fribidi"
+  "/usr/local/Cellar/harfbuzz/2.6.6/include/harfbuzz"
+  "/usr/local/Cellar/graphite2/1.3.14/include"
+  "/usr/local/Cellar/atk/2.36.0/include/atk-1.0"
+  "/usr/local/Cellar/pixman/0.40.0/include/pixman-1"
+  "/usr/local/Cellar/fontconfig/2.13.1/include"
+  "/usr/local/opt/freetype/include/freetype2"
+  "/usr/local/Cellar/libpng/1.6.37/include/libpng16"
+  "/usr/local/Cellar/gdk-pixbuf/2.40.0_1/include/gdk-pixbuf-2.0"
+  "/usr/local/Cellar/glib/2.64.3/include"
+  "/usr/local/Cellar/glib/2.64.3/include/glib-2.0"
+  "/usr/local/Cellar/glib/2.64.3/lib/glib-2.0/include"
+  "/usr/local/opt/gettext/include"
+  "/usr/local/Cellar/pcre/8.44/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TP2_Operativos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP2_Operativos.dir/main.c.o"
+  "TP2_Operativos"
+  "TP2_Operativos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP2_Operativos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
